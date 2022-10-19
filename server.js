2384980c6dedfdccb34ae7a77d7d03d136db987a
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-10-19 17:51:11 +0300

    update mongoose connect

--- server.js
+++ server.js
@@ -3,13 +3,36 @@ const mongoose = require("mongoose");
 
 const { DB_HOST, PORT = 4000 } = process.env;
 
-mongoose
-  .connect(DB_HOST)
-  .then(() => {
+const appListenAsync = (port) =>               // === Промисифицированая  app.listen ==
+  new Promise((resolve, reject) => {
+    app.listen(port, (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
+  });
+
+const mongooseConnect = async () => {
+  try {
+    mongoose.connect(DB_HOST);
     console.log("Database connection successful");
-    app.listen(PORT);
-  })
-  .catch((error) => {
+    await appListenAsync(PORT);
+    console.log(`Server run on port ${PORT}`);
+  } catch (error) {
     console.log(error.message);
     process.exit(1);
-  });
+  }
+};
+mongooseConnect();
+
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() => {
+//     console.log("Database connection successful");
+//     app.listen(PORT);
+//   })
+//   .catch((error) => {
+//     console.log(error.message);
+//     process.exit(1);
+//   });
